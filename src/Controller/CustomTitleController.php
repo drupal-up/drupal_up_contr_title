0e14f3c129033723c8107bf6bda36298b593be5d
flesheater <nikolay.r.borisov@gmail.com>
2018-05-21 16:20:10 +0200

    Initial commit.

---
+++ src/Controller/CustomTitleController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\drupal_up_contr_title\Controller;
+
+/**
+ * Controller with custom title callback.
+ */
+class CustomTitleController {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function dummyPage() {
+
+    return [
+      '#type' => 'markup',
+      '#markup' => 'Hello from our controller',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function titleCallback() {
+    $randTitleId = rand(0, 2);
+    $randTitles = [
+      'This is a great page',
+      'This is an alful page',
+      'Is this a page?',
+    ];
+
+    return $randTitles[$randTitleId];
+  }
+
+}
